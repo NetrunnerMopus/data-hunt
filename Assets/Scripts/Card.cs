25060055c5d287ad7dbf80036deab29ad7e0f9d7
Maciej Kwidziński <dagguh@users.noreply.github.com>
2017-10-14 20:01:38 +0200

    Basic draggable card

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Card : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+       
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+    
+    }
+}
---
+++ Assets/Scripts/Netrunner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Netrunner : MonoBehaviour
+{
+
+    public Canvas canvas;
+    public EventSystem eventSystem;
+
+    private int cards = 0;
+
+    void Start()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            CreateCard();
+        }
+
+    }
+
+    private void CreateCard()
+    {
+        cards++;
+        GameObject gameObject = new GameObject("Card #" + cards);
+        gameObject.transform.SetParent(canvas.transform);
+
+
+        gameObject.layer = 5;
+
+        Image image = gameObject.AddComponent<Image>();
+        image.sprite = Resources.Load<Sprite>("Images/Cards/11024");
+        image.preserveAspect = true;
+
+
+        RectTransform rectangle = gameObject.GetComponent<RectTransform>();
+
+        rectangle.anchorMin = new Vector2(0.2f, 0.4f);
+        rectangle.anchorMax = new Vector2(0.2f, 0.4f);
+        rectangle.anchoredPosition = new Vector2(cards * 90.0f, cards * 10.0f);
+        rectangle.pivot = new Vector2(0.5f, 0.5f);
+        rectangle.sizeDelta = new Vector2(300, 419);
+
+        Card card = gameObject.AddComponent<Card>();
+    }
+
+}
