fa8d6f93034ef086d7a5158fd3f92e8668d43e29
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2017-10-22 23:05:58 +0200

    Add Process Automation and Build Script

---
+++ Assets/Scripts/Cards/ProcessAutomation.cs
@@ -0,0 +1,19 @@
+﻿public class ProcessAutomation : ICard
+{
+    string ICard.GetImageAsset()
+    {
+        return "process-automation";
+    }
+
+    string ICard.GetName()
+    {
+        return "Process Automation";
+    }
+
+    bool ICard.PlayFromGrip()
+    {
+        Netrunner.game.runner.creditPool.Gain(2);
+        Netrunner.game.runner.stack.Draw();
+        return true;
+    }
+}
--- Assets/Scripts/Decks.cs
+++ Assets/Scripts/Decks.cs
@@ -5,10 +5,12 @@ public class Decks
     public Deck DemoRunner()
     {
         var cards = new List<ICard>();
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < 5; i++)
         {
             cards.Add(new Diesel());
             cards.Add(new SureGamble());
+            cards.Add(new BuildScript());
+            cards.Add(new ProcessAutomation());
         }
         return new Deck(cards);
     }
