a7e7e9f9e248d037db16959e64039ae185bce5b5
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2018-06-01 12:27:16 +0200

    Split `RunnerPaidAbilityWindowTest` to two test cases: timing test and effect test

--- Assets/Tests/PaidAbilityWindowTest.cs
+++ Assets/Tests/PaidAbilityWindowTest.cs
@@ -6,65 +6,89 @@ using model.cards.runner;
 using tests.observers;
 using tests.mocks;
 using view.log;
+using model.play;
+using model.timing;
 
 namespace tests
 {
     public class PaidAbilityWindowTest
     {
-        [Test, Timeout(1000)]
-        public void ShouldPopHopper()
+        private Game game;
+        private PassiveCorp passiveCorp;
+        private Ability runnerAction;
+        private PaidAbilityObserver paidAbilityObserver;
+        private SportsHopper hopper;
+        private PaidWindow window;
+
+        [SetUp]
+        public void SetUp()
         {
             var runnerCards = new List<ICard>();
             for (int i = 0; i < 20; i++)
             {
                 runnerCards.Add(new Diesel());
             }
-            var game = new Game(new Decks().DemoCorp(), new Deck(runnerCards));
+            game = new Game(new Decks().DemoCorp(), new Deck(runnerCards));
             var gameFlowLog = new GameFlowLog();
             gameFlowLog.Display(game);
-            var passiveCorp = new PassiveCorp(game);
-            var grip = game.runner.zones.grip;
-            var heap = game.runner.zones.heap;
-            var rig = game.runner.zones.rig;
-            var actionCard = game.runner.actionCard;
-            var clickForCredit = actionCard.credit;
+            passiveCorp = new PassiveCorp(game);
+            runnerAction = game.runner.actionCard.credit;
+            paidAbilityObserver = new PaidAbilityObserver();
+            hopper = new SportsHopper();
+            window = game.flow.paidWindow;
+            window.ObserveAbility(paidAbilityObserver);
+        }
+
+        [Test, Timeout(1000)]
+        public void ShouldPopHopper()
+        {
+            game.Start();
+            passiveCorp.SkipTurn();
+            var zones = game.runner.zones;
+            zones.grip.Add(hopper);
             var gripObserver = new GripObserver();
             var rigObserver = new RigObserver();
             var heapObserver = new HeapObserver();
-            var paidAbilityObserver = new PaidAbilityObserver();
-            heap.Observe(heapObserver);
-            rig.ObserveUninstallations(rigObserver);
-            var hopper = new SportsHopper();
-            var window = game.flow.paidWindow;
-            window.ObserveAbility(paidAbilityObserver);
+            zones.grip.ObserveAdditions(gripObserver);
+            zones.rig.ObserveUninstallations(rigObserver);
+            zones.heap.Observe(heapObserver);
+            game.runner.actionCard.Install(hopper).Trigger(game);
+            var popHopper = paidAbilityObserver.NewestPaidAbility;
 
+            popHopper.Trigger(game);
+
+            Assert.AreEqual(3, gripObserver.TotalAdded);
+            Assert.AreEqual(hopper, rigObserver.LastUninstalled);
+            Assert.AreEqual(hopper, heapObserver.LastAdded);
+        }
+
+        [Test, Timeout(1000)]
+        public void ShouldUsePaidAbilityOnRunnerTurn()
+        {
             game.Start();
             passiveCorp.SkipTurn();
-            clickForCredit.Trigger(game);
-            clickForCredit.Trigger(game);
-            grip.Add(hopper);
-            actionCard.Install(hopper).Trigger(game);
+            game.runner.zones.grip.Add(hopper);
+
+            runnerAction.Trigger(game);
+            runnerAction.Trigger(game);
+            game.runner.actionCard.Install(hopper).Trigger(game);
             var popHopper = paidAbilityObserver.NewestPaidAbility;
             window.Pass();
-            clickForCredit.Trigger(game);
+            runnerAction.Trigger(game);
             window.Pass();
             window.Pass();
             passiveCorp.SkipTurnIgnoringWindows();
             window.Pass();
             window.Pass();
             window.Pass();
-            clickForCredit.Trigger(game);
+            runnerAction.Trigger(game);
             window.Pass();
-            clickForCredit.Trigger(game);
-            grip.ObserveAdditions(gripObserver);
+            runnerAction.Trigger(game);
+
             popHopper.Trigger(game);
             window.Pass();
-            clickForCredit.Trigger(game);
-            clickForCredit.Trigger(game);
-
-            Assert.AreEqual(3, gripObserver.TotalAdded);
-            Assert.AreEqual(hopper, rigObserver.LastUninstalled);
-            Assert.AreEqual(hopper, heapObserver.LastAdded);
+            runnerAction.Trigger(game);
+            runnerAction.Trigger(game);
         }
     }
 }
