228667305baf33310bc5a18551640849f8d0ecc9
Maciej Kwidzinski <dagguh@gmail.com>
2020-05-17 17:28:11 +0200

    Fix drop-highlight transparency

--- Assets/Scripts/Controller/DropZone.cs
+++ Assets/Scripts/Controller/DropZone.cs
@@ -9,9 +9,11 @@ namespace controller
         private Image Image { get { return GetComponent<Image>(); } }
         private Color Color { set { Image.color = value; } }
         private bool droppableDragged;
+        private Color originalColor = Color.magenta;
 
         void Start()
         {
+            originalColor = Image.color;
             StopDragging();
         }
 
@@ -61,9 +63,7 @@ namespace controller
 
         private void ResetHighlights()
         {
-            var reset = Color.white;
-            reset.a = 0.1f;
-            Color = reset;
+            Color = originalColor;
         }
     }
 }
