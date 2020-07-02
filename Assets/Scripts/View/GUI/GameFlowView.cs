f676fc7faa5f6983a8dd798003b4a431a71b7056
Maciej Kwidzinski <dagguh@gmail.com>
2020-07-02 21:56:22 +0200

    Stop the old clicks rendering

--- Assets/Scripts/View/GUI/GameFlowView.cs
+++ Assets/Scripts/View/GUI/GameFlowView.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using model;
 using model.timing;
-using UnityEngine.UI;
 
 namespace view.gui
 {
@@ -14,12 +13,8 @@ namespace view.gui
         public void Display(GameObject board, Game game)
         {
             flow = CreateFlow();
-            var corpPool = CreateCorpClicks(game);
-            var runnerPool = CreateRunnerClicks(game);
             PaidWindow = CreatePaidWindow(game.runner.paidWindow);
             var gameFinish = CreateGameFinish(game);
-            corpPool.AttachTo(flow);
-            runnerPool.AttachTo(flow);
             flow.AttachTo(board);
             gameFinish.AttachTo(board);
         }
@@ -33,42 +28,6 @@ namespace view.gui
             rectangle.offsetMin = Vector2.zero;
             rectangle.offsetMax = Vector2.zero;
             return view;
-        }
-
-        private GameObject CreateCorpClicks(Game game)
-        {
-            var view = new GameObject("Corp clicks");
-            var rectangle = view.AddComponent<RectTransform>();
-            rectangle.anchorMin = new Vector2(0.0f, 1.0f - clickRowHeightRatio);
-            rectangle.anchorMax = new Vector2(1.0f, 1.0f);
-            rectangle.offsetMin = Vector2.zero;
-            rectangle.offsetMax = Vector2.zero;
-            AddHorizontalLayout(view);
-            game.corp.clicks.Observe(view.AddComponent<ClickPoolRow>());
-            return view;
-        }
-
-        private GameObject CreateRunnerClicks(Game game)
-        {
-            var view = new GameObject("Runner clicks");
-            var rectangle = view.AddComponent<RectTransform>();
-            rectangle.anchorMin = new Vector2(0.0f, 0.0f);
-            rectangle.anchorMax = new Vector2(1.0f, clickRowHeightRatio);
-            rectangle.offsetMin = Vector2.zero;
-            rectangle.offsetMax = Vector2.zero;
-            AddHorizontalLayout(view);
-            game.runner.clicks.Observe(view.AddComponent<ClickPoolRow>());
-            return view;
-        }
-
-        private void AddHorizontalLayout(GameObject gameObject)
-        {
-            var layout = gameObject.AddComponent<HorizontalLayoutGroup>();
-            layout.childForceExpandWidth = true;
-            layout.childForceExpandHeight = true;
-            layout.childControlWidth = true;
-            layout.childControlHeight = true;
-            layout.childAlignment = TextAnchor.MiddleLeft;
         }
 
         private PaidWindowView CreatePaidWindow(PaidWindow window)
