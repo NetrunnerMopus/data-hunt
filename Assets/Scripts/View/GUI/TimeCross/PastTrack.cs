5e16b15e6b70f3c8270e51929b28d56f05a15fa9
Maciej Kwidzinski <dagguh@gmail.com>
2020-07-08 23:54:40 +0200

    Delete unused click-pool GUI code

--- Assets/Scripts/View/GUI/TimeCross/PastTrack.cs
+++ Assets/Scripts/View/GUI/TimeCross/PastTrack.cs
@@ -9,7 +9,7 @@ using model.timing.runner;
 
 namespace view.gui.timecross
 {
-    public class PastTrack : MonoBehaviour, IClickObserver, ICorpActionObserver, IRunnerActionObserver
+    public class PastTrack : MonoBehaviour, ICorpActionObserver, IRunnerActionObserver
     {
         private Sprite clickSprite;
         private HorizontalLayoutGroup horizontal;
@@ -25,47 +25,6 @@ namespace view.gui.timecross
             horizontal.childControlHeight = true;
             horizontal.childForceExpandWidth = false;
             horizontal.childForceExpandHeight = true;
-        }
-
-        void IClickObserver.NotifyClicks(int spent, int remaining)
-        {
-            var desired = spent;
-            AddMissing(desired);
-            RemoveExtra(desired);
-        }
-
-        private void AddMissing(int desired)
-        {
-            while (renderedClicks.Count < desired)
-            {
-                Render();
-            }
-        }
-
-        private void RemoveExtra(int desired)
-        {
-            var extra = renderedClicks.Count - desired;
-            if (extra > 0)
-            {
-                foreach (var click in renderedClicks.GetRange(0, extra))
-                {
-                    Destroy(click);
-                    renderedClicks.Remove(click);
-                }
-            }
-        }
-
-        private void Render()
-        {
-            var click = ClickBox.RenderClickBox(gameObject);
-            var aspect = click.AddComponent<AspectRatioFitter>();
-            aspect.aspectRatio = 1;
-            aspect.aspectMode = AspectRatioFitter.AspectMode.HeightControlsWidth;
-            horizontal.CalculateLayoutInputHorizontal();
-            horizontal.CalculateLayoutInputVertical();
-            horizontal.SetLayoutHorizontal();
-            horizontal.SetLayoutVertical();
-            renderedClicks.Add(click);
         }
 
         void ICorpActionObserver.NotifyActionTaking()
