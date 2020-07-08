eb4530b07f103f1956ea5cf4ced4ca4e18dbdc58
Maciej Kwidzinski <dagguh@gmail.com>
2020-07-08 22:52:09 +0200

    Pulse highlight with lime color
    The static cyan highlight was colliding with "daylight" time track.

--- Assets/Scripts/View/GUI/Highlight.cs
+++ Assets/Scripts/View/GUI/Highlight.cs
@@ -7,18 +7,28 @@ namespace view.gui
     {
         private Image Image { get { return GetComponent<Image>(); } }
         private Color Color { set { Image.color = value; } }
+        private bool on = false;
+        private Color highlight = Color.yellow * 0.7f + Color.green * 0.8f;
+        private Color reset = Color.white + new Color(0f, 0f, 0f, 1f);
 
         public void TurnOn()
         {
-            var on = Color.cyan;
-            Color = on;
+            on = true;
+        }
+
+        void Update()
+        {
+            if (on)
+            {
+                var phase = Mathf.Sin(Time.time * 4);
+                Color = Color.Lerp(highlight, reset, phase);
+            }
         }
 
         public void TurnOff()
         {
-            var off = Color.white;
-            off.a = 1.0f;
-            Color = off;
+            on = false;
+            Color = reset;
         }
     }
 }
