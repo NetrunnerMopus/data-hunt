45ed23e6330f53a2a7b7b717630abe790bb928ee
Maciej Kwidzinski <dagguh@gmail.com>
2020-07-18 13:58:09 +0200

    Avoid corp "daylight" color in dropzone highlights
    Reuse the flashy highlight in dropzones.

--- Assets/Scripts/View/GUI/Highlight.cs
+++ Assets/Scripts/View/GUI/Highlight.cs
@@ -8,8 +8,8 @@ namespace view.gui
         private Image Image { get { return GetComponent<Image>(); } }
         private Color Color { set { Image.color = value; } }
         private bool on = false;
-        private Color highlight = Color.yellow * 0.7f + Color.green * 0.8f;
-        private Color reset = Color.white + new Color(0f, 0f, 0f, 1f);
+        public Color Flash = Color.yellow * 0.7f + Color.green * 0.8f;
+        public Color Rest = Color.white + new Color(0f, 0f, 0f, 1f);
 
         public void TurnOn()
         {
@@ -21,14 +21,14 @@ namespace view.gui
             if (on)
             {
                 var phase = Mathf.Sin(Time.time * 4);
-                Color = Color.Lerp(highlight, reset, phase);
+                Color = Color.Lerp(Flash, Rest, phase);
             }
         }
 
         public void TurnOff()
         {
             on = false;
-            Color = reset;
+            Color = Rest;
         }
     }
 }
