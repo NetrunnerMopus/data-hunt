01061dc4dd88a5422f10b7ef0a34dc91159853d6
Maciej Kwidzinski <dagguh@gmail.com>
2020-08-12 21:40:42 +0200

    Add a main menu

---
+++ Assets/Scripts/Controller/Menu/GameMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    public void PlaySoloRunner()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
