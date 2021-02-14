80d0c645945aa2560b88b21947a0565a5bed49cf
Maciej Kwidzinski <dagguh@gmail.com>
2021-02-14 19:03:00 +0100

    Add symmetry between IStealOption and ITrashOption

--- Assets/Scripts/Model/Choices/Trash/ITrashOption.cs
+++ Assets/Scripts/Model/Choices/Trash/ITrashOption.cs
@@ -5,7 +5,8 @@ namespace model.choices.trash
     public interface ITrashOption
     {
         bool IsLegal(Game game);
-        Task Perform(Game game);
+        /// <returns>true if card was trashed</returns>
+        Task<bool> Perform(Game game);
         string Art { get; }
     }
 }
