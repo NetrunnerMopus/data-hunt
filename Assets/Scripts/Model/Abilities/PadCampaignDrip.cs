1331e906abeda16a2c886537399f829445cf3e47
Maciej Kwidzinski <dagguh@gmail.com>
2022-06-13 20:01:18 +0200

    Try experimental API for PAD Campaign

--- Assets/Scripts/Model/Abilities/PadCampaignDrip.cs
+++ Assets/Scripts/Model/Abilities/PadCampaignDrip.cs
@@ -1,8 +1,24 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using model.play;
 
 namespace model.abilities {
     public class PadCampaignDrip : IAbility {
+        public ISource Source => throw new System.NotImplementedException();
+
+        public bool Active => throw new System.NotImplementedException();
+
+        public Task Resolve() {
+            throw new System.NotImplementedException();
+        }
+
+        public Condition TriggerCondition() {
+            return YourTurnBegins();
+        }
+
+        public PadCampaignDripInstance CreatePending() {
+            NextReactionWindow().Add(new PadCampaignDripInstance())
+        }
     }
 }
